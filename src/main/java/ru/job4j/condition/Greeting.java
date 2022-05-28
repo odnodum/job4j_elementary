3b48df8dcd7f32cee39457fbe495c2880a9a896c
Ramil Khanov <h-ram@yandex.ru>
2022-05-28 22:55:39 +0300

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -2,11 +2,19 @@ package ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
-        String idea = "I like Java!";
-        System.out.println(idea);
-        idea += " But I am a newbie. ";
-        int year = 2022;
-        idea += year;
-        System.out.println(idea);
+//        String idea = "I like Java!";
+//        System.out.println(idea);
+//        idea += " But I am a newbie. ";
+//        int year = 2022;
+//        idea += year;
+//        System.out.println(idea);
+        // 2 выражение
+        // 3 выражение
+        // 4 выражение
+        // 5 выражение
+        short s = 1500;
+        char c = (char) s;
+        double d = c;
+        System.out.println(d);
     }
 }
--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -16,5 +16,17 @@ public class Converter {
         float dollar = Converter.rubleToDollar(240);
         System.out.println("140 rubles are " + euro + " euro.");
         System.out.println("240 rubles are " + dollar + " dollar.");
+
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+
+        float in2 = 240;
+        float expected2 = 4;
+        float out2 = Converter.rubleToDollar(in2);
+        boolean passed2 = expected2 == out2;
+        System.out.println("240 rubles are 4. Test result : " + passed2);
     }
 }
