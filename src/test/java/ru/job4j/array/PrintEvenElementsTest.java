6e4cdc295a452d6f2150a28bdf097f529dd5b816
Ramil Khanov <h-ram@yandex.ru>
2022-05-31 21:18:52 +0300

    6.4.1.1. Обход массива с последнего элемента [#361619]

--- src/test/java/ru/job4j/array/PrintEvenElementsTest.java
+++ src/test/java/ru/job4j/array/PrintEvenElementsTest.java
@@ -9,7 +9,7 @@ public class PrintEvenElementsTest {
     public void whenArrayHasOddNumberOfElements() {
         String ln = System.lineSeparator();
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-        String expected = "элементы массива с чётными индексами, начиная с последнего: " + ln
+        String expected = "Элементы массива с чётными индексами, начиная с последнего: " + ln
                 + 15 + ln + 13 + ln + 11 + ln + 9 + ln + 7 + ln + 5 + ln + 3 + ln + 1 + ln;
         assertEquals(expected, PrintEvenElements.printEvenInts(numbers));
     }
@@ -18,7 +18,7 @@ public class PrintEvenElementsTest {
     public void whenArrayHasEvenNumberOfElements() {
         String ln = System.lineSeparator();
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
-        String expected = "элементы массива с чётными индексами, начиная с последнего: " + ln
+        String expected = "Элементы массива с чётными индексами, начиная с последнего: " + ln
                 + 13 + ln + 11 + ln + 9 + ln + 7 + ln + 5 + ln + 3 + ln + 1 + ln;
         assertEquals(expected, PrintEvenElements.printEvenInts(numbers));
     }
