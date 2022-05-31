94b1f144462c57e18d26a4fcb3066231ebace7c2
Ramil Khanov <h-ram@yandex.ru>
2022-05-31 17:58:36 +0300

    6.1. Классический поиск перебором. [#33489 #307309]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -2,13 +2,11 @@ package ru.job4j.array;
 
 public class FindLoop {
     public static int indexOf(int[] data, int el) {
-        int rst = -1;
         for (int i = 0; i < data.length; i++) {
             if (data[i] == el) {
-                rst = i;
-                break;
+                return i;
             }
         }
-        return rst;
+        return -1;
     }
 }
