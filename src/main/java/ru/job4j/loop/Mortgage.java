af1a8d49437f8517ca06a73a34abd07008e84d0a
Ramil Khanov <h-ram@yandex.ru>
2022-05-31 18:01:24 +0300

    5.7. Ипотека [#156315 #307292]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,10 +4,9 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
         percent = percent / 100;
-        amount = amount + amount * percent;
         while (amount > 0) {
-            amount = amount - salary;
             amount = amount + amount * percent;
+            amount = amount - salary;
             year++;
         }
         return year;
